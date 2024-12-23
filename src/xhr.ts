48750c2df5ddf14649183eb8b4867ea2a2e5ad7a
doooghan <robinhaaan@gmail.com>
2024-12-23 17:16:25 +0800

    feat: 获取相应数据-1

--- src/xhr.ts
+++ src/xhr.ts
@@ -1,19 +1,43 @@
-import { AxiosRequestConfig } from "./types";
+import { AxiosPromise, AxiosRequestConfig, AxiosResponse } from "./types";
 
-export default function xhr(config: AxiosRequestConfig): void {
-  const { data = null, method = "get", url, headers } = config;
+export default function xhr(config: AxiosRequestConfig): AxiosPromise {
+  return new Promise((resolve) => {
+    const { data = null, method = "get", url, headers, responseType } = config;
 
-  const request = new XMLHttpRequest();
+    const request = new XMLHttpRequest();
 
-  request.open(method.toLocaleUpperCase(), url, true);
-
-  Object.keys(headers).forEach((name) => {
-    if (data === null && name.toLowerCase() === "content-type") {
-      delete headers[name];
-    } else {
-      request.setRequestHeader(name, headers[name]);
+    if (responseType) {
+      request.responseType = responseType;
     }
-  });
 
-  request.send(data);
+    request.open(method.toLocaleUpperCase(), url, true);
+
+    request.onreadystatechange = (event) => {
+      if (request.readyState !== 4) {
+        return;
+      }
+
+      const responseHeader = request.getAllResponseHeaders();
+      const responseData = responseType && responseType === "text" ? request.responseText : request.response;
+      const response: AxiosResponse = {
+        data: responseData,
+        status: request.status,
+        statusText: request.statusText,
+        headers: responseHeader,
+        config,
+        request,
+      };
+      resolve(response);
+    };
+
+    Object.keys(headers).forEach((name) => {
+      if (data === null && name.toLowerCase() === "content-type") {
+        delete headers[name];
+      } else {
+        request.setRequestHeader(name, headers[name]);
+      }
+    });
+
+    request.send(data);
+  });
 }
